d3399f5f2e7dfb216d5393c27accf1402c968a3c
798748 <42977374+798748@users.noreply.github.com>
2019-02-12 10:20:49 -0800

    SPEED BOOST

--- MySwingApplication/CanvasComponent.java
+++ MySwingApplication/CanvasComponent.java
@@ -21,6 +21,7 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     int motionSpeed = 1;
 
     public CanvasComponent(int w, int h){
+        //makes the square
         setSize(w, h);
         width = w;
         height = h;
@@ -32,13 +33,29 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
 
     public void keyTyped(KeyEvent e){
         char keyChar = e.getKeyChar();
+        //resets the square if it gets too crazy
         if (keyChar == 'r'){
             rectX = 0;
             rectY = 0;
         }
+        //makes the square go in a random direction
         if (keyChar == 'q'){
             animationDeltaX = (int)(Math.random() * 3) - 1;
             animationDeltaY = (int)(Math.random() * 3) - 1;
+        }
+        //makes the square to stop
+        if (keyChar == 's'){
+            motionSpeed = 0;
+        }
+        //makes the square to speed up or reset if it's too fast
+        if (keyChar == 'w'){
+            if(motionSpeed == 1){
+                motionSpeed = 25;  
+            }else if(motionSpeed == 25){
+                motionSpeed = 50;
+            }else{
+                motionSpeed = 1;
+            }
         }
     }
 
@@ -51,17 +68,18 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     }
 
     public void actionPerformed(ActionEvent e){
+        //makes the square move around the edge of the box
         Dimension componentSizeDimension = getSize();
-        if (rectX + width > getWidth()){
-            rectX = getWidth() - width;
-            animationDeltaX = 0;
-            animationDeltaY = 1;
-            rectY += animationDeltaY * motionSpeed;
-        }else if (rectY + height > getHeight()){
+        /*if (rectY + height > getHeight()){
             rectY = getHeight() - height;
             animationDeltaX = -1;
             animationDeltaY = 0;
             rectX += animationDeltaY * motionSpeed;
+        }else if (rectX + width > getWidth()){
+            rectX = getWidth() - width;
+            animationDeltaX = 0;
+            animationDeltaY = 1;
+            rectY += animationDeltaY * motionSpeed;
         }else if (rectX < 0){
             rectX = 0;
             animationDeltaX = 0;
@@ -75,11 +93,36 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
         }else{
             rectX += animationDeltaX * motionSpeed;
             rectY += animationDeltaY * motionSpeed;
+        }*/
+        if (rectY + height > getHeight()){
+            rectY = getHeight() - height;
+            animationDeltaX = -1;
+            animationDeltaY = 0;
+            rectX += -animationDeltaY * motionSpeed;
+        }else if (rectX + width > getWidth()){
+            rectX = getWidth() - width;
+            animationDeltaX = 0;
+            animationDeltaY = 1;
+            rectY += -animationDeltaY * motionSpeed;
+        }else if (rectX < 0){
+            rectX = 0;
+            animationDeltaX = 0;
+            animationDeltaY = -1;
+            rectY += -animationDeltaY * motionSpeed;
+        }else if (rectY < 0){
+            rectY = 0;
+            animationDeltaX = 1;
+            animationDeltaY = 0;
+            rectY += -animationDeltaY * motionSpeed;
+        }else{
+            rectX += animationDeltaX * motionSpeed;
+            rectY += animationDeltaY * motionSpeed;
         }
         repaint();
     }
 
     public void paintComponent(Graphics g){
+        //colors the square
         g.setColor(Color.green);
         g.fillRect(rectX, rectY, width, height);
     }
@@ -89,6 +132,7 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     }
 
     public void mousePressed(MouseEvent e){
+        //checks if the cursor is on the square
         mouseFromX = e.getX();
         mouseFromY = e.getY();
         if (mouseFromX >= rectX && mouseFromX <= rectX+width && mouseFromY >= rectY && mouseFromY <= rectY+height){
@@ -97,6 +141,7 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     }
 
     public void mouseReleased(MouseEvent e){
+        //lets the square stop being crazy
         motionSpeed = 1;
     }
 
@@ -109,7 +154,8 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     }
 
     public void mouseDragged(MouseEvent e){
-        if(shapeSelected){
+        //lets the square go crazy if you hold it
+        if (shapeSelected){
             motionSpeed ++;
         }
     }
--- MySwingApplication/MySwingApplication.java
+++ MySwingApplication/MySwingApplication.java
@@ -8,17 +8,19 @@ import javax.swing.*;
 public class MySwingApplication implements Runnable{
     JFrame jFrame;
     CanvasComponent canvasComponent;
-    
+
     public void run(){
         jFrame = new JFrame("Window");
-        jFrame.setSize(400, 400);
-        canvasComponent = new CanvasComponent(100, 100);
+        jFrame.setSize(800, 400);
+        for(int i = 2; i > 0; i--){
+            canvasComponent = new CanvasComponent(100, 100);
+        }
         jFrame.add(canvasComponent);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
         jFrame.addKeyListener(canvasComponent);
     }
-    
+
     public static void main(){
         MySwingApplication mySwingApplicationSwing = new MySwingApplication();
         javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
