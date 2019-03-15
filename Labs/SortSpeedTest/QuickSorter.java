633609d0a554035bbaf198933fe44316d83ef66f
798748 <42977374+798748@users.noreply.github.com>
2019-03-15 10:24:57 -0700

    Formatting Mostly

--- Labs/SortSpeedTest/QuickSorter.java
+++ Labs/SortSpeedTest/QuickSorter.java
@@ -5,64 +5,62 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class QuickSorter extends Sorter
-{
+public class QuickSorter extends Sorter{
 
     /**
      * Constructor for objects of class QuickSorter
      */
-    public QuickSorter()
-    {
+    public QuickSorter(){
         super();
     }
 
-    public void onePass1(int[] a, int initialLeftIndex, int limitRightIndex) {
+    public void onePass1(int[] a, int initialLeftIndex, int limitRightIndex){
         int pivotValue = a[initialLeftIndex];
         int rightIndex = limitRightIndex - 1;
         int leftIndex = initialLeftIndex + 1;
-        while (leftIndex <= rightIndex) {
-            if (less(pivotValue, a[leftIndex])) {
-                while ((rightIndex >= leftIndex) && less(pivotValue, a[rightIndex])) {
+        while (leftIndex <= rightIndex){
+            if (less(pivotValue, a[leftIndex])){
+                while ((rightIndex >= leftIndex) && less(pivotValue, a[rightIndex])){
                     rightIndex--;
                 }
-                if (leftIndex < rightIndex) {
+                if (leftIndex < rightIndex){
                     swap(a, leftIndex, rightIndex);
                     rightIndex--;
                     leftIndex++;
                 }
-            } else {
+            } else{
                 leftIndex++;
             }
         }
-        if (initialLeftIndex < leftIndex - 1) {
+        if (initialLeftIndex < leftIndex - 1){
             swap(a, initialLeftIndex, leftIndex - 1);
             onePass(a, initialLeftIndex, leftIndex - 1);
         }
-        if (leftIndex < limitRightIndex) {
+        if (leftIndex < limitRightIndex){
             onePass(a, leftIndex, limitRightIndex);
         }
     }
 
-    private void setPivot(int[] a, int initialLeftIndex, int limitRightIndex) {
+    private void setPivot(int[] a, int initialLeftIndex, int limitRightIndex){
         int elementCount = limitRightIndex - initialLeftIndex;
         int rightIndex = limitRightIndex - 1;
-        if (elementCount >= 3) {
+        if (elementCount >= 3){
             int middleIndex = initialLeftIndex + elementCount/2;
             int leftValue = a[initialLeftIndex];
             int rightValue = a[rightIndex];
             int middleValue = a[middleIndex];
-            if (less(a[rightIndex], a[middleIndex])) {
-                if (less(a[initialLeftIndex], a[rightIndex])) {
+            if (less(a[rightIndex], a[middleIndex])){
+                if (less(a[initialLeftIndex], a[rightIndex])){
                     swap(a, initialLeftIndex, rightIndex);
                 }
             }
-            if (less(a[initialLeftIndex], a[middleIndex])) {
+            if (less(a[initialLeftIndex], a[middleIndex])){
                 swap(a, initialLeftIndex, middleIndex);
             }
         }
     }
 
-    public void onePass(int[] a, int initialLeftIndex, int limitRightIndex) {
+    public void onePass(int[] a, int initialLeftIndex, int limitRightIndex){
         // By eliminating the smart pivot code, sorted arrays produce
         // worst case behavior
         // setPivot(a, initialLeftIndex, limitRightIndex);
@@ -70,28 +68,28 @@ public class QuickSorter extends Sorter
         int rightIndex = limitRightIndex - 1;
         int leftIndex = initialLeftIndex + 1;
         while (leftIndex <= rightIndex) {
-            while ((leftIndex <= rightIndex) && lessEqual(a[leftIndex], pivotValue)) {
+            while ((leftIndex <= rightIndex) && lessEqual(a[leftIndex], pivotValue)){
                 leftIndex++;
             }
-            while ((leftIndex <= rightIndex) && less(pivotValue, a[rightIndex])) {
+            while ((leftIndex <= rightIndex) && less(pivotValue, a[rightIndex])){
                 rightIndex--;
             }
-            if (leftIndex < rightIndex) {
+            if (leftIndex < rightIndex){
                 swap(a, leftIndex, rightIndex);
                 rightIndex--;
                 leftIndex++;
             }
         }
-        if (initialLeftIndex < leftIndex - 1) {
+        if (initialLeftIndex < leftIndex - 1){
             swap(a, initialLeftIndex, leftIndex - 1);
             onePass(a, initialLeftIndex, leftIndex - 1);
         }
-        if (leftIndex < limitRightIndex) {
+        if (leftIndex < limitRightIndex){
             onePass(a, leftIndex, limitRightIndex);
         }
     }
 
-    public void sort(int[] a) {
+    public void sort(int[] a){
         onePass(a, 0, a.length);
     }
 }
--- Labs/SortSpeedTest/SortRunner.java
+++ Labs/SortSpeedTest/SortRunner.java
@@ -5,22 +5,21 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class SortRunner
-{
-    static int[] lengthArray = { 10, 100, 1000, 10000 };
+public class SortRunner{
+    static int[] lengthArray ={10, 100, 1000};
     static boolean printArrayFlag = false;
 
-    public static boolean isSorted(int[] a) {
-        for (int index = 1; index < a.length; index++) {
+    public static boolean isSorted(int[] a){
+        for (int index = 1; index < a.length; index++){
             if (a[index] < a[index-1])
                 return false;
         }
         return true;
     }
 
-    public static void printArray(int[] a) {
+    public static void printArray(int[] a){
         System.out.print("[");
-        for (int index = 0; index < a.length; index++) {
+        for (int index = 0; index < a.length; index++){
             System.out.print(" " + a[index] + " ");
         }
         System.out.println("]");
@@ -28,14 +27,14 @@ public class SortRunner
 
     static int[] columnIndexArray = {40, 50, 68, 86, 104, 122 };
 
-    public static String advanceToColumnIndex(String string, int columnIndex) {
-        while (string.length() < columnIndex) {
+    public static String advanceToColumnIndex(String string, int columnIndex){
+        while (string.length() < columnIndex){
             string += " ";
         }
         return string;
     }
 
-    public static void printHeader() {
+    public static void printHeader(){
         String resultString = "";
         resultString = advanceToColumnIndex(resultString, columnIndexArray[0]);
         resultString += "length";
@@ -46,13 +45,13 @@ public class SortRunner
         resultString = advanceToColumnIndex(resultString, columnIndexArray[3]);
         resultString += "extraSpaceCount";
         resultString = advanceToColumnIndex(resultString, columnIndexArray[4]);
-        //         resultString += "recursiveCall";
-        //         resultString = advanceToColumnIndex(resultString, columnIndexArray[5]);
+        //resultString += "recursiveCall";
+        //resultString = advanceToColumnIndex(resultString, columnIndexArray[5]);
         resultString += "isSorted returns";
         System.out.println(resultString);
     }
 
-    public static void printScaledResult(Sorter sorter, int[] a, String prefixString, double divisor) {
+    public static void printScaledResult(Sorter sorter, int[] a, String prefixString, double divisor){
         String resultString = prefixString;
         resultString = advanceToColumnIndex(resultString, columnIndexArray[1]);
         resultString += String.format("%4.2f", (sorter.comparisonCount/divisor)) + ",";
@@ -61,15 +60,15 @@ public class SortRunner
         resultString = advanceToColumnIndex(resultString, columnIndexArray[3]);
         resultString += String.format("%4.2f", (sorter.extraSpaceCount/divisor)) + ",";
         resultString = advanceToColumnIndex(resultString, columnIndexArray[4]);
-        //         resultString += sorter.recursiveCallCount;
-        //         while (resultString.length() < columnIndexArray[5]) {
-        //             resultString += " ";
-        //         }
+        //resultString += sorter.recursiveCallCount;
+        //while (resultString.length() < columnIndexArray[5]) {
+        //resultString += " ";
+        //}
         resultString += isSorted(a);
         System.out.println(resultString);
     }
 
-    public static void printResult(Sorter sorter, int[] a, String prefixString) {
+    public static void printResult(Sorter sorter, int[] a, String prefixString){
         String resultString = prefixString;
         resultString = advanceToColumnIndex(resultString, columnIndexArray[0]);
         resultString += a.length + ",";
@@ -80,10 +79,10 @@ public class SortRunner
         resultString = advanceToColumnIndex(resultString, columnIndexArray[3]);
         resultString += sorter.extraSpaceCount + ",";
         resultString = advanceToColumnIndex(resultString, columnIndexArray[4]);
-        //         resultString += sorter.recursiveCallCount;
-        //         while (resultString.length() < columnIndexArray[5]) {
-        //             resultString += " ";
-        //         }
+        //resultString += sorter.recursiveCallCount;
+        //while (resultString.length() < columnIndexArray[5]) {
+        //resultString += " ";
+        //}
         resultString += isSorted(a);
         System.out.println(resultString);
     }
@@ -100,7 +99,7 @@ public class SortRunner
     public static final int RANDOM_ARRAY_INDEX = 2;
     public static final int MAX_ARRAY_INDEX = 3;
 
-    public static void printOneStatistic(Sorter[][] outerSorterArray, int statId, String name) {
+    public static void printOneStatistic(Sorter[][] outerSorterArray, int statId, String name){
         if (outerSorterArray[0] == null) {
             return; // runStandardSorts not executed for this outerSorterArray 
         }
@@ -117,8 +116,8 @@ public class SortRunner
         resultString = advanceToColumnIndex(resultString, columnIndexArray[4]);
         resultString += "c/(length*log(length))";
         System.out.println(resultString);
-        for (int innerIndex = SORTED_ARRAY_INDEX; innerIndex <= RANDOM_ARRAY_INDEX; innerIndex++) {
-            for (int outerIndex = 0; outerIndex < lengthArray.length; outerIndex++) {
+        for (int innerIndex = SORTED_ARRAY_INDEX; innerIndex <= RANDOM_ARRAY_INDEX; innerIndex++){
+            for (int outerIndex = 0; outerIndex < lengthArray.length; outerIndex++){
                 Sorter[] innerSorterArray = outerSorterArray[outerIndex];
                 Sorter sorter = innerSorterArray[innerIndex];
                 resultString = name + " - " + arrayNameArray[innerIndex];
@@ -126,7 +125,7 @@ public class SortRunner
                 resultString += sorter.arrayLength + ",";
                 long longStat = 0;
                 double arrayLength = sorter.arrayLength;
-                switch (statId) {
+                switch (statId){
                     case COMPARISON_STAT_ID:
                     longStat = sorter.comparisonCount;
                     break;
@@ -152,51 +151,50 @@ public class SortRunner
                 System.out.println(resultString);
 
             }
-
         }
     }
 
-    public static void runOneSort(Sorter sorter, int[] a, String prefixString) {
+    public static void runOneSort(Sorter sorter, int[] a, String prefixString){
         sorter.reset(a);
-        if (printArrayFlag) {
+        if (printArrayFlag){
             System.out.println(prefixString + " - pre sort");
             printArray(a);
         }
         sorter.sort(a);
         sorter.done();
-        if (printArrayFlag) {        
+        if (printArrayFlag){        
             System.out.println(prefixString + "  - post sort");
             printArray(a);
         }
-        //         printResult(sorter, a, prefixString);
-        //         printScaledResult(sorter, a, "        / length", a.length);
-        //         printScaledResult(sorter, a, "        / length^2", a.length*a.length);
-        //         printScaledResult(sorter, a, "        / length*log(length)", a.length*Math.log(a.length));
-        //         System.out.println(prefixString + "\tlength: " + a.length + ",\tisSorted returns " + isSorted(a));
+        //printResult(sorter, a, prefixString);
+        //printScaledResult(sorter, a, "        / length", a.length);
+        //printScaledResult(sorter, a, "        / length^2", a.length*a.length);
+        //printScaledResult(sorter, a, "        / length*log(length)", a.length*Math.log(a.length));
+        //System.out.println(prefixString + "\tlength: " + a.length + ",\tisSorted returns " + isSorted(a));
     }
 
-    public static int[] createSortedArray(int length) {
+    public static int[] createSortedArray(int length){
         int[] resultArray = new int[length];
         for (int index = 0; index < length; index++) 
             resultArray[index] = index + 1;
         return resultArray;
     }
 
-    public static int[] createReverseSortedArray(int length) {
+    public static int[] createReverseSortedArray(int length){
         int[] resultArray = new int[length];
         for (int index = 0; index < length; index++) 
             resultArray[index] = length - index;
         return resultArray;
     }
 
-    public static int[] createRandomSortedArray(int length) {
+    public static int[] createRandomSortedArray(int length){
         int[] resultArray = new int[length];
         for (int index = 0; index < length; index++) 
             resultArray[index] = (int)(Math.random() * length);
         return resultArray;
     }
 
-    public static Sorter[] runStandardSorts(Sorter sorter, int length, String name) {
+    public static Sorter[] runStandardSorts(Sorter sorter, int length, String name){
         Sorter[] resultSorterArray = new Sorter[MAX_ARRAY_INDEX];        
         runOneSort(sorter, createSortedArray(length), name + " - " + arrayNameArray[SORTED_ARRAY_INDEX]);
         resultSorterArray[SORTED_ARRAY_INDEX] = sorter.clone();
@@ -207,37 +205,37 @@ public class SortRunner
         return resultSorterArray;
     }
 
-    public static void printStandardResults(Sorter[][] outerSorterArray, String name) {
+    public static void printStandardResults(Sorter[][] outerSorterArray, String name){
         printOneStatistic(outerSorterArray, COMPARISON_STAT_ID, name);
         printOneStatistic(outerSorterArray, MOVE_STAT_ID, name);
         printOneStatistic(outerSorterArray, EXTRA_SPACE_STAT_ID, name);
-//        printOneStatistic(outerSorterArray, ELAPSED_TIME_STAT_ID, name);
+        //printOneStatistic(outerSorterArray, ELAPSED_TIME_STAT_ID, name);
     }
 
-    public static void main() {
-        //         printHeader();
+    public static void main(){
+        //printHeader();
         Sorter[][] selectionSorterArray = new Sorter[lengthArray.length][];
         Sorter[][] insertionSorterArray = new Sorter[lengthArray.length][];
         Sorter[][] mergeSorterArray = new Sorter[lengthArray.length][];
         Sorter[][] quickSorterArray = new Sorter[lengthArray.length][];
         Sorter[][] heapSorterArray = new Sorter[lengthArray.length][];
-        for (int lengthIndex = 0; lengthIndex < lengthArray.length; lengthIndex++) {
+        for (int lengthIndex = 0; lengthIndex < lengthArray.length; lengthIndex++){
             int length = lengthArray[lengthIndex];
-            //             SelectionSorter selectionSorter = new SelectionSorter();
-            //             selectionSorterArray[lengthIndex] = runStandardSorts(selectionSorter, length, "Selection sort");
-            //             InsertionSorter insertionSorter = new InsertionSorter();
-            //             insertionSorterArray[lengthIndex] = runStandardSorts(insertionSorter, length, "Insertion sort");
-            //             MergeSorter mergeSorter = new MergeSorter();
-            //             mergeSorterArray[lengthIndex] = runStandardSorts(mergeSorter, length, "Merge sort");
-            //             HeapSorter heapSorter = new HeapSorter();
-            //             heapSorterArray[lengthIndex] = runStandardSorts(heapSorter, length, "Heap sort");
+            //SelectionSorter selectionSorter = new SelectionSorter();
+            //selectionSorterArray[lengthIndex] = runStandardSorts(selectionSorter, length, "Selection sort");
+            //InsertionSorter insertionSorter = new InsertionSorter();
+            //insertionSorterArray[lengthIndex] = runStandardSorts(insertionSorter, length, "Insertion sort");
+            //MergeSorter mergeSorter = new MergeSorter();
+            //mergeSorterArray[lengthIndex] = runStandardSorts(mergeSorter, length, "Merge sort");
+            //HeapSorter heapSorter = new HeapSorter();
+            //heapSorterArray[lengthIndex] = runStandardSorts(heapSorter, length, "Heap sort");
             QuickSorter quickSorter = new QuickSorter();
             quickSorterArray[lengthIndex] = runStandardSorts(quickSorter, length, "Quick sort");
         }
-        // printStandardResults(selectionSorterArray, "Selection sort");
-        // printStandardResults(insertionSorterArray, "Insertion sort");
-        // printStandardResults(mergeSorterArray, "Merge sort");
-        // printStandardResults(heapSorterArray, "Heap sort");
+        //printStandardResults(selectionSorterArray, "Selection sort");
+        //printStandardResults(insertionSorterArray, "Insertion sort");
+        //printStandardResults(mergeSorterArray, "Merge sort");
+        //printStandardResults(heapSorterArray, "Heap sort");
         printStandardResults(quickSorterArray, "Quick sort");    
     }
 }
