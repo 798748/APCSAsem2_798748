10339d55c3ba192f63353825e34b1ae344b710d2
798748 <42977374+798748@users.noreply.github.com>
2019-03-19 10:19:01 -0700

    ¯\_(ツ)_/¯

---
+++ FRQ 2008_2/StringPart.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class StringPart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StringPart{
+    
+
+    /**
+     * Constructor for objects of class StringPart
+     */
+    public StringPart(){
+        
+    }
+}
